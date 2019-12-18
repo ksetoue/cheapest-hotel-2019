2ceda017237312209652f2dbda66c106a4eb439a
ksetoue <karoline.kimiko@gmail.com>
2019-12-17 21:10:56 -0300

    Add test project and ReservationTest

---
+++ BookingTest/ReservationHelperTest.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Booking.Reservations;
+using Booking.Hotels;
+using Utils;
+using Xunit;
+
+namespace BookingTests
+{
+    public class ReservationTest
+    {
+        [Theory]
+        [InlineData("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)", "Lakewood")]
+        [InlineData("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)", "Bridgewood")]
+        [InlineData("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)", "Ridgewood")]
+        public void GetCheapestHotel_ValidInput_ReturnCheapestHotelName(string input, string expected)
+        {
+            //Arrange
+            ReservationRequest reservationRequest = InputParser.BuildReservationRequest(input);
+            IList<Hotel> defaultHotels = DefaultHotels.GetDefaultHotels();
+
+            //Act
+            var result = Reservation
+                .GetCheapestHotel(reservationRequest, defaultHotels)
+                .Hotel
+                .Name;
+
+            //Assert
+            Assert.Equal(result, expected);
+        }
+
+    }
+
+}
