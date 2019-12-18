529a2104136a15c8d522ba340fab6d3d1056314e
ksetoue <karoline.kimiko@gmail.com>
2019-12-17 21:20:44 -0300

    Add enum for week day controll

---
+++ Booking/WeekDay/WeekDay.cs
@@ -0,0 +1,16 @@
+namespace Booking.Dates
+{
+    /// <summary>
+    /// enum that describes each week day
+    /// </summary>
+    public enum WeekDay
+    {
+        mon = 1,
+        tues = 2,
+        wed = 3,
+        thur = 4,
+        fri = 5,
+        sat = 6,
+        sun = 7
+    }
+}
