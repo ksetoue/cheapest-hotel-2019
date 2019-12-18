719f423b32e82b600f331c65007a0875e70eeb22
ksetoue <karoline.kimiko@gmail.com>
2019-12-17 22:07:25 -0300

    Test class for console application

---
+++ ApplicationCoreTest/ApplicationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ApplicationCoreTest
+{
+    public class ApplicationCoreTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
